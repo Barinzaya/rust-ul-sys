8014b2f41ef29d6dba085e88ce4ccb641b789443
Barinzaya <barinzaya@gmail.com>
2022-07-23 11:57:26 -0400

    Stopped assuming the library search path.
    The build script will now use a `UL_PATH` environment variable, rather than assuming a Linux-like path, allowing for use with a pre-built distribution.

--- build.rs
+++ build.rs
@@ -1,23 +1,23 @@
-use bindgen::builder;
-
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rustc-flags=-L /usr/local/lib/");
-    println!("cargo:rustc-link-search=/usr/local/lib/");
+    if let Some(p) = env::var_os("UL_PATH") {
+        let ul_path = PathBuf::from(p);
 
-    if cfg!(target_os = "macos") {
-        println!("cargo:rustc-link-lib=dylib=Ultralight");
-        println!("cargo:rustc-link-lib=dylib=WebCore");
-        println!("cargo:rustc-link-lib=dylib=AppCore");
+        let lib_path = ul_path.join("lib");
+        println!("cargo:rustc-link-search={}", lib_path.display());
     } else {
-        println!("cargo:rustc-link-lib=Ultralight");
-        println!("cargo:rustc-link-lib=WebCore");
-        println!("cargo:rustc-link-lib=AppCore");
+        println!("cargo:rustc-link-search=/usr/local/lib");
     }
 
-    let bindings = bindgen::Builder::default()
+    println!("cargo:rustc-link-lib=dylib=Ultralight");
+    println!("cargo:rustc-link-lib=dylib=WebCore");
+    println!("cargo:rustc-link-lib=dylib=AppCore");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    bindgen::Builder::default()
         .header("wrapper/wrapper.h")
         .impl_debug(true)
         .impl_partialeq(true)
@@ -27,11 +27,7 @@ fn main() {
         .whitelist_type("^UL.*|JS.*|ul.*|WK.*")
         .whitelist_function("^UL.*|JS.*|ul.*|WK.*")
         .generate()
-        .expect("Unable to generate bindings");
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-
-    bindings
+        .expect("Unable to generate bindings")
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }
